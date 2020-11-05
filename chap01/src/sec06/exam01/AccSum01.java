502a8e1d7f638707f664b92701a4283b82ac2566
tjoeun-jg-501 <tjoeun-jg-501@DESKTOP-4IU9U8O>
2020-11-05 11:56:41 +0900

    수열의 합 AccSum01 /*Sum=1+2+3+4+...+NUM   NUM(10)항까지의 합  i   1  2  3  4       1  1  1*/ AccSum06 /*6) Sum=2+3+5+8+12+17+?...+NUM 항까지의 합 	       i	 2  3  5  8 12 	       j   1  2  3  4*/

---
+++ chap01/src/sec06/exam01/AccSum01.java
@@ -0,0 +1,19 @@
+package sec06.exam01;
+/*Sum=1+2+3+4+...+NUM   NUM(10)항까지의 합
+ i   1  2  3  4
+      1  1  1*/
+public class AccSum01 {
+	public static void main(String[] args) {
+		int sum = 0, i=0; //기준치를 과정을 생각하고 나중에 잡아라
+		int cnt;
+		for(cnt=1; cnt<=10; cnt++) {
+			i=i+1;
+			sum = sum+i; //컴퓨터가 결과를 나타내기전에 과정은 결과치 전에 작성하는것이 좋음.
+			System.out.printf("cnt=%3d, i=%2d, sum=%3d\n", cnt, i, sum); //%2d는 자리수를 두자리 준거. 오른쪽 기준으로 자리가 잡힘
+			
+		}
+		System.out.printf("====반복문종료=====\n");
+		System.out.printf("cnt=%2d, i=%2d, sum=%3d\n", cnt, i, sum);
+	}
+	
+}
---
+++ chap01/src/sec06/exam01/AccSum06.java
@@ -0,0 +1,22 @@
+package sec06.exam01;
+/*6) Sum=2+3+5+8+12+17+?...+NUM 항까지의 합
+	       i	 2  3  5  8 12
+	       j   1  2  3  4*/
+public class AccSum06 {
+
+	public static void main(String[] args) {
+		int sum = 0, i=2, j=0; //기준치를 과정을 생각하고 나중에 잡아라
+		int cnt;
+		for(cnt=1; cnt<=10; cnt++) {
+			sum = sum+i; //컴퓨터가 결과를 나타내기전에 과정은 결과치 전에 작성하는것이 좋음.
+			j=j+1;
+			System.out.printf("cnt=%3d, j=%2d, i=%2d, sum=%3d\n", cnt, j, i, sum); //%2d는 자리수를 두자리 준거. 오른쪽 기준으로 자리가 잡힘
+			i=j+i;
+			
+			
+		}
+		System.out.printf("====반복문종료=====\n");
+		System.out.printf("cnt=%2d,  j=%2d, i=%2d, sum=%3d\n", cnt, j, i, sum);
+	}
+
+}
