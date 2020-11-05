f690ca82b2826c2711255b4a8e7b278db5bebd49
tjoeun-jg-501 <tjoeun-jg-501@DESKTOP-4IU9U8O>
2020-11-05 17:43:44 +0900

    짝수단는 1부터 홀수단은 9부터 출력하는 구구단. if문 없이 2중 for문으로 표현

--- chap01/src/sec06/exam01/GooGoodan.java
+++ chap01/src/sec06/exam01/GooGoodan.java
@@ -1,25 +1,27 @@
 package sec06.exam01;
-
+//짝수단는 1부터 홀수단은 9부터 출력하는 구구단. if문 없이 for문 중첩으로 표현
 public class GooGoodan {
 
 	public static void main(String[] args) {
 		int dan = 0;
 		int times = 0;
 		int value = 0;
-		
+		int st =1;
+		int end = 9;
+		int sw =1, temp;
 		for(dan = 2; dan<=9; dan++) {
-			if (dan%2==0) {
-				for(times = 1; times<= 9; times++) {
-					value = dan*times;
-					System.out.println(dan + "X" +times + "=" + value);
-				}
-			}else{
-				for(times = 9; times>=1; times--) {
-				value = dan*times;
-				System.out.println(dan + "X" +times + "=" + value);
-				}
-			}
-			System.out.printf("====="+dan+"단종료=====\n");
+				for(times = st; times != end+sw; times=times+sw) { 
+					//for(times = end; times != 1+(-sw); times=times+(-sw)) { //그냥 증감과 하향을 표현하기위한 표현법. 이 줄은 없어야 맞는거.
+						value = dan*times;
+						System.out.println(dan + "X" +times + "=" + value);
+					}
+				System.out.printf("====="+dan+"단종료=====\n");
+				temp = st;
+				st = end;
+				end=temp;
+				sw = sw*(-1);
+					
+		//	}
 		}
 	}
 }
