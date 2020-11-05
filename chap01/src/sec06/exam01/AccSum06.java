f11e588f794f0ffb520aad135242ffca7b6b31ea
tjoeun-jg-501 <tjoeun-jg-501@DESKTOP-4IU9U8O>
2020-11-05 12:53:19 +0900

    가장 최선의 답. sum전에 계산 끝내기. 11줄과12줄 바꾸면 이상해짐

--- chap01/src/sec06/exam01/AccSum06.java
+++ chap01/src/sec06/exam01/AccSum06.java
@@ -8,12 +8,10 @@ public class AccSum06 {
 		int sum = 0, i=2, j=0; //기준치를 과정을 생각하고 나중에 잡아라
 		int cnt;
 		for(cnt=1; cnt<=10; cnt++) {
+			i=j+i;
 			j=j+1;
-			
 			sum = sum+i; //컴퓨터가 결과를 나타내기전에 과정은 결과치 전에 작성하는것이 좋음.
 			System.out.printf("cnt=%3d, j=%2d, i=%2d, sum=%3d\n", cnt, j, i, sum); //%2d는 자리수를 두자리 준거. 오른쪽 기준으로 자리가 잡힘
-			i=j+i;
-			
 			
 		}
 		System.out.printf("====반복문종료=====\n");
