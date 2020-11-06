bc5a3076a677f5cc84e02a91f9adbe838badd199
tjoeun-jg-501 <tjoeun-jg-501@DESKTOP-4IU9U8O>
2020-11-06 09:21:24 +0900

    NUM(10)항까지의 합. 빠져나가는 값의 확인

--- chap01/src/sec06/exam01/AccSum01.java
+++ chap01/src/sec06/exam01/AccSum01.java
@@ -9,8 +9,7 @@ public class AccSum01 {
 		for(cnt=1; cnt<=10; cnt++) {
 			i=i+1;
 			sum = sum+i; //컴퓨터가 결과를 나타내기전에 과정은 결과치 전에 작성하는것이 좋음.
-			System.out.printf("cnt=%3d, i=%2d, sum=%3d\n", cnt, i, sum); //%2d는 자리수를 두자리 준거. 오른쪽 기준으로 자리가 잡힘
-			
+			System.out.printf("cnt=%3d, i=%2d, sum=%3d\n", cnt, i, sum); //%2d는 자리수를 두자리 준거. 오른쪽 기준으로 자리가 잡힘.
 		}
 		System.out.printf("====반복문종료=====\n");
 		System.out.printf("cnt=%2d, i=%2d, sum=%3d\n", cnt, i, sum);//cnt는 11로 빠져나감.cnt=11, i=10, sum= 55
