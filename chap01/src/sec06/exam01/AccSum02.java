a0fd29971051a1ebaf4f3b2a0a8d1fb2c3cf864a
tjoeun-jg-501 <tjoeun-jg-501@DESKTOP-4IU9U8O>
2020-11-06 09:44:48 +0900

    홀수의 합

--- chap01/src/sec06/exam01/AccSum02.java
+++ chap01/src/sec06/exam01/AccSum02.java
@@ -1,5 +1,6 @@
 package sec06.exam01;
 //Sum=1+3+5+...+NUM 항까지의 합(홀수의 합)
+
 public class AccSum02 {
 	public static void main(String[] args) {
 		int sum=1, i=1 ;
@@ -8,7 +9,6 @@ public class AccSum02 {
 			i=i+2;
 			sum = sum+i;
 			System.out.printf("cnt=%3d, i=%2d, sum=%3d\n", cnt, i, sum); 
-			
 		}
 		System.out.printf("====반복문종료=====\n");
 		System.out.printf("cnt=%2d, i=%2d, sum=%3d\n", cnt, i, sum);
