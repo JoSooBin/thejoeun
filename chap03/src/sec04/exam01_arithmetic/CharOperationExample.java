500f392e77f0dc5a5ea53ebd035644910ea57fb9
tjoeun-jg-501 <tjoeun-jg-501@DESKTOP-4IU9U8O>
2020-11-09 18:16:23 +0900

    컴파일 에러(char로 선언된 값은 int와 더할 수 없다.

--- chap03/src/sec04/exam01_arithmetic/CharOperationExample.java
+++ chap03/src/sec04/exam01_arithmetic/CharOperationExample.java
@@ -4,9 +4,9 @@ public class CharOperationExample {
 	public static void main(String[] args) {
 		char c1 = 'A' + 1;
 		char c2 = 'A';
-		//char c3 = c2 + 1;    //컴파일 에러
-		System.out.println("c1: " + c1);
-		System.out.println("c2: " + c2);
-		//System.out.println("c3: " + c3);
+		char c3 = (char)(((int)c2 + 1));    //컴파일 에러
+		System.out.println("c1: " + c1); //c1: B
+		System.out.println("c2: " + c2); //c2: A
+		System.out.println("c3: " + c3);
 	}
 }
